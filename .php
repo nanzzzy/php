61cdbcaa5bed480dba041214d865a1e142e90065
Nancy hesham <31672650+nanzzzy@users.noreply.github.com>
2017-10-11 23:29:23 +0200

    Update .php

--- .php
+++ .php
@@ -6,12 +6,20 @@
 
 </head>
 <body>
-    $info = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
-
-foreach($info as $x => $x_value) {
-    echo "Key=" . $x . ", Value=" . $x_value;
-    echo "<br>";
-}
+    <?php
+$cars = array
+  (
+  array("physics",19,45),
+  array("chmestry",18,47),
+  array("biolgy",17,44),
+  array("math",15,49)
+  );
+  
+echo $cars[0][0].": medteam: ".$cars[0][1].", final: ".$cars[0][2].".<br>";
+echo $cars[1][0].": medteam: ".$cars[1][1].", final: ".$cars[1][2].".<br>";
+echo $cars[2][0].": medteam: ".$cars[2][1].", final: ".$cars[2][2].".<br>";
+echo $cars[3][0].": medteam: ".$cars[3][1].", final: ".$cars[3][2].".<br>";
+?>
     
 </body>
 
