04e64c6afb5a8eb44d74bbeb0736514d44c3c1ac
Nancy hesham <31672650+nanzzzy@users.noreply.github.com>
2017-10-11 23:42:20 +0200

    Update .php

--- .php
+++ .php
@@ -9,16 +9,10 @@
     <?php
 $cars = array
   (
-  array("physics",19,45),
-  array("chmestry",18,47),
-  array("biolgy",17,44),
-  array("math",15,49)
+  array("physics",19,45,"chmestry",18,47,"biolgy",17,44,"math",15,49)
   );
   
 echo $cars[0][0].": medteam: ".$cars[0][1].", final: ".$cars[0][2].".<br>";
-echo $cars[1][0].": medteam: ".$cars[1][1].", final: ".$cars[1][2].".<br>";
-echo $cars[2][0].": medteam: ".$cars[2][1].", final: ".$cars[2][2].".<br>";
-echo $cars[3][0].": medteam: ".$cars[3][1].", final: ".$cars[3][2].".<br>";
 ?>
     
 </body>
