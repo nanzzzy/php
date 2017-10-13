ec35ec2f24a5ede06e707cddd7ad4332199d121e
Nancy hesham <31672650+nanzzzy@users.noreply.github.com>
2017-10-13 12:31:46 +0200

    Create welcom.php

---
+++ welcom.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<form action="welcome_get.php" method="get">
+Name: <input type="text" name="name"><br>
+message: <input type="text" name="message"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
